5c76404e92c64c99e4f129fe8c92d384fc672716
JT Archie <jtarchie@gmail.com>
2020-11-25 11:04:49 -0700

    go fmt

--- commands/staged_ops_manager.go
+++ commands/staged_ops_manager.go
@@ -10,31 +10,31 @@ type StagedOpsManager struct{}
 
 var payloads = api.Payloads{
 	"syslog-settings": api.Payload{
-		Endpoint: "/api/v0/settings/syslog",
-		Root:     "syslog",
+		Endpoint:      "/api/v0/settings/syslog",
+		Root:          "syslog",
 		IsCollectable: true,
 	},
 	"banner-settings": api.Payload{
-		Endpoint: "/api/v0/settings/banner",
+		Endpoint:      "/api/v0/settings/banner",
 		IsCollectable: true,
 	},
 	"rbac-settings": api.Payload{
-		Endpoint: "/api/v0/settings/rbac",
+		Endpoint:      "/api/v0/settings/rbac",
 		IsCollectable: false,
 	},
 	"pivotal-network-settings": api.Payload{
-		Endpoint: "/api/v0/settings/pivotal_network_settings",
-		Root:     "pivotal_network_settings",
+		Endpoint:      "/api/v0/settings/pivotal_network_settings",
+		Root:          "pivotal_network_settings",
 		IsCollectable: false,
 	},
 	"ssl-certificate": api.Payload{
-		Endpoint: "/api/v0/settings/ssl_certificate",
+		Endpoint:      "/api/v0/settings/ssl_certificate",
 		IsCollectable: true,
 	},
 	"uaa-tokens-expiration": api.Payload{
-		Endpoint: "/api/v0/uaa/tokens_expiration",
+		Endpoint:      "/api/v0/uaa/tokens_expiration",
 		IsCollectable: true,
-		Root: "tokens_expiration",
+		Root:          "tokens_expiration",
 	},
 }
 
